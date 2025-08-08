2fd400c2b0ad0a929de5257777dbb220881a3a86
Your Name <deekshashetti96@gmail.com>
2025-08-08 17:34:13 +0530

    added a navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import './App.css';
 import Home from "./page/home";      // Capitalized to match file name
 import Favorites from './page/favorites';
 import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
+    <div>
+      <NavBar />
     <main className="main-content">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </main>
+    </div>
   );
 }
 
