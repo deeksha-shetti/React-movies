e732740d0175dee910ccda9f996845f3f7d36bf9
Your Name <deekshashetti96@gmail.com>
2025-08-08 21:39:22 +0530

    after styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './css/App.css';
 import Home from "./page/home";      // Capitalized to match file name
 import Favorites from './page/favorites';
 import { Routes, Route } from 'react-router-dom';
